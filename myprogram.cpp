b8f8f0486c4f419b9abeccc939b98db47884f61c
Khamikova-Marina <153535824+Khamikova-Marina@users.noreply.github.com>
2024-03-11 12:27:46 +0300

    2

---
+++ myprogram.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "hello.hpp"
+
+int main(int argc, char* argv[]) {
+    first();
+    second();
+
+    std::cout << "Main function..." << std::endl;
+
+    return 0;
+}
---
+++ newprogram.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "hello.hpp"
+
+int main(int argc, char* argv[]) {
+    first();
+    //second();
+
+    std::cout << "Main function 2..." << std::endl;
+
+    return 0;
+}
